4b07bced97c531496002834d9ff446b18d992933
Paulaszka <pkropkasz@gmail.com>
2024-03-14 20:57:12 +0100

    Dodaj pliki projektów.

---
+++ MyTestProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+using MyProject;
+
+namespace MyTestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
