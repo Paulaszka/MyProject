80aea1282c8e8e0df4460e2cea423ba2aebb3bff
Paulaszka <pkropkasz@gmail.com>
2024-05-31 19:14:49 +0200

    datatest fixed

--- DataTest/DataAPITest.cs
+++ DataTest/DataAPITest.cs
@@ -1,4 +1,5 @@
 using Data;
+using System.Numerics;
 
 namespace DataTest
 {
@@ -9,14 +10,15 @@ namespace DataTest
         [TestMethod]
         public void ConstructorTest()
         {
-            DataAbstractAPI dataAbstractAPI = DataAbstractAPI.CreateApi(100, 100);
-            dataAbstractAPI.CreateBallsList(5);
-            Assert.AreEqual(3, dataAbstractAPI.GetBall(2).BallId);
-            Assert.AreEqual(5, dataAbstractAPI.GetAmount);
-            dataAbstractAPI.CreateBallsList(-3);
-            Assert.AreEqual(2, dataAbstractAPI.GetAmount);
-            dataAbstractAPI.CreateBallsList(-3);
-            Assert.AreEqual(0, dataAbstractAPI.GetAmount);
+            Position position1 = new(100, 200);
+            Vector2 vector = new(5, 10);
+            DataAbstractAPI ball = DataAbstractAPI.CreateApi(20, position1, vector, 30);
+            Assert.AreEqual(20, ball.BallSize);
+            Assert.AreEqual(100, ball.BallPosition.X);
+            Assert.AreEqual(200, ball.BallPosition.Y);
+            Assert.AreEqual(5, ball.Velocity.X);
+            Assert.AreEqual(10, ball.Velocity.Y);
+            Assert.AreEqual(30, ball.BallWeight);
         }
     }
 }
