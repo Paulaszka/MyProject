403143e19134e98a1f3d4274c05157420b34a2ce
Paulaszka <pkropkasz@gmail.com>
2024-06-02 01:05:50 +0200

    added logger files

---
+++ Data/BallLoggerAPI.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public abstract class BallLoggerAPI
+    {
+        public abstract Position BallPosition { get; set; }
+        public abstract Vector2 Velocity { get; set; }
+
+        public static BallLoggerAPI CreateBallLogger(Position _position, Vector2 _velocity, DateTime _datetime)
+        {
+            return new BallLogger(_position, _velocity, _datetime);
+        }
+    }
+
+    internal class BallLogger : BallLoggerAPI
+    {
+        private Position position;
+        private Vector2 velocity;
+        private DateTime datetime;
+        private readonly object positionLock = new();
+        private readonly object velocityLock = new();
+
+        public BallLogger(Position _position, Vector2 _velocity, DateTime _datetime)
+        {
+            position = _position;
+            velocity = _velocity;
+            datetime = _datetime;
+        }
+
+        public override Position BallPosition
+        {
+            get => position;
+            set
+            {
+                lock (positionLock)
+                {
+                    position = value;
+                }
+            }
+        }
+
+        public override Vector2 Velocity
+        {
+            get => velocity;
+            set
+            {
+                lock (velocityLock)
+                {
+                    velocity = value;
+                }
+            }
+        }
+    }
+}
---
+++ Data/LoggerAPI.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    abstract class LoggerAPI
+    {
+        public abstract void AddBallToQueue(DataAbstractAPI ball);
+
+        public LoggerAPI CreateLogger() 
+        {
+            return new Logger();
+        }
+    }
+
+    internal class Logger : LoggerAPI
+    {
+        private readonly string FileName = "./logfile";
+        private readonly int maxQueue = 1000;
+        private bool IsQueueFull = false;
+
+        private readonly ConcurrentQueue<BallLoggerAPI> ConcurrentQueue;
+
+        private Task _loggingTask;
+        public Logger()
+        {
+            //return new Logger();
+        }
+
+        public override void AddBallToQueue(DataAbstractAPI ball)
+        {
+            Task.Run(() =>
+            {
+                if (ConcurrentQueue.Count >= maxQueue && IsQueueFull == false)
+                {
+                    IsQueueFull = true;
+                }
+                else
+                {
+                    Position _position = new(ball.BallPosition.X, ball.BallPosition.Y);
+                    Vector2 _velocity = new Vector2(ball.Velocity.X, ball.Velocity.Y);
+                    ConcurrentQueue.Enqueue(BallLoggerAPI.CreateBallLogger(_position, _velocity, DateTime.Now));
+                }   
+            });
+        }
+    }
+}
